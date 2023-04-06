8f6a17c1acabfea435a1dfd2ce19dece82b66b9b
EssaadaniYounes <younesessaadani2003@gmail.com>
2023-04-06 18:26:34 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace splitPdf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtWord = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Separate Word:";
+            // 
+            // txtWord
+            // 
+            this.txtWord.Location = new System.Drawing.Point(184, 51);
+            this.txtWord.Name = "txtWord";
+            this.txtWord.Size = new System.Drawing.Size(271, 26);
+            this.txtWord.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 240);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 52);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Chose File and save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(184, 131);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(271, 26);
+            this.txtPath.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "The Folder path :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 319);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtWord);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtWord;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System;
+using System.Windows.Forms;
+using IronPdf;
+using System.IO;
+
+namespace splitPdf
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+        public string splice(string item, int index)
+        {
+            string result = "";
+            for (int i = index; i < item.Length; i++)
+            {
+                result += item[i];
+            }
+            return result;
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                string Path = "";
+                OpenFileDialog openFile = new OpenFileDialog();
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                    Path = openFile.FileName;
+                }
+                PdfSharp.Pdf.PdfDocument pdf = PdfReader.Open(Path, PdfDocumentOpenMode.Import);
+
+                for (int i = 0; i < pdf.PageCount; i++)
+                {
+
+
+                    PdfSharp.Pdf.PdfDocument document = new PdfSharp.Pdf.PdfDocument();
+                    document.AddPage(pdf.Pages[i]);
+
+                    MemoryStream stream = new MemoryStream();
+                    document.Save(stream);
+                    byte[] pdfBytes = stream.ToArray();
+
+                    IronPdf.PdfDocument pdfDocument = new IronPdf.PdfDocument(pdfBytes);
+
+                    string Text = pdfDocument.ExtractAllText();
+                    string[] lines = Text.Split('\n');
+                    foreach (string line in lines)
+                    {
+                        string item = "";
+                        for (int j = 0; j < line.Length; j++)
+                        {
+                            item += line[j];
+                            if (item == txtWord.Text)
+                            {
+                                string fileName = line.Substring(j, (line.Length - j));
+                                fileName = fileName.TrimEnd('\r', '\n');
+                                if (fileName[1] == '0')
+                                {
+                                    fileName = splice(fileName, 2);
+                                }
+                                PdfSharp.Pdf.PdfDocument FinalFile = new PdfSharp.Pdf.PdfDocument();
+                                FinalFile.AddPage(pdf.Pages[i]);
+                                FinalFile.Save(txtPath.Text + "\" + fileName.ToString() + ".pdf");
+                            }
+                        }
+                    }
+                }
+                MessageBox.Show("Saved");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
